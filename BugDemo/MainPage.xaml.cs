4f9ffb880f136eb4d3b3b11de4e2d75beedf1494
Oliver Brown <oliver.brown@xdesign.com>
2019-09-05 16:01:32 +0100

    Initial commit.

---
+++ BugDemo/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BugDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateItemsSource();
+        }
+
+        private void UpdateItemsSource()
+        {
+            list.ItemsSource = null;
+            list.ItemsSource = new List<ViewModel>
+            {
+                new ViewModel { Group = "Group A", Content = "Content A1" },
+                new ViewModel { Group = "Group A", Content = "Content A2" },
+                new ViewModel { Group = "Group B", Content = "Content B1" },
+                new ViewModel { Group = "Group B", Content = "Content B2" },
+            };
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            UpdateItemsSource();
+        }
+    }
+
+    public class ViewModel
+    {
+        public string Content { get; set; }
+        public string Group { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Setting ItemsSource to null (and subsequently setting it to not-null) causes grouping to stop working.
+
+iOS (which seems to have transparent cells by default) seems to suggest that grouping is happening but the group headers have a height of 0.
